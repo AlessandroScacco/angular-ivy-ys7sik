781c2d1ab9d77bc11d67a9b946d5bc7c21ebf86a
Alessandro Scacco <alessandro.scacco@sysdata.it>
2022-07-16 17:09:12 +0200

    refactoring

--- src/app/pages/post/post.component.ts
+++ src/app/pages/post/post.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { PostService } from '../../service/api/post.service';
 
 @Component({
   selector: 'app-post',
@@ -8,16 +7,12 @@ import { PostService } from '../../service/api/post.service';
 })
 export class PostComponent implements OnInit {
 
-  constructor(private postService: PostService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.posts();
+    
   }
 
-  private posts(): void {
-    this.postService.getPosts().subscribe(k=>{
-      console.warn('Response : ', k);
-    }); 
-  }
+  
 
 }
