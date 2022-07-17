e05372a21652c9af0f35c24ce5e591b09fa8543b
Alessandro Scacco <alessandro.scacco@sysdata.it>
2022-07-17 19:08:19 +0200

    fix ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,15 @@
-import { Component } from '@angular/core';
-import { pic, picMod1, picMod2 } from '../assets/exercise';
-import { TranslateUtilService } from './services/translate-util/translate-util.service';
+import { Component } from "@angular/core";
+import { TranslateUtilService } from "./services/translate-util/translate-util.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  pic = pic;
-  picMod1 = picMod1;
-  picMod2 = picMod2;
-
   constructor(translateService: TranslateUtilService) {
     translateService.initializeTranslate();
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 }
